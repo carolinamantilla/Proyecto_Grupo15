11da78feb836d9a4dcfb6ffa8c4cd212fb72d403
unknown <carolina.mantilla14@gmail.com>
2021-10-02 01:43:07 -0500

    Interfaz login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import {Container} from 'reactstrap';
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ return(
+  <Container>
+      <div className="App">
+        <b /><b />
+        <GoogleLogin 
+        clientId="892636235404-l62fs4fq03m2eds9okkqskfdb5v2i10q.apps.googleusercontent.com"
+        buttonText="Iniciar Sesión"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        />
+      </div>
+      
+      
+    </Container>
   );
 }
 
