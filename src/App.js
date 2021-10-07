f6c3d8d1e68f4850a6b1bced334b0e5e27a9ec1c
unknown <carolina.mantilla14@gmail.com>
2021-10-06 22:17:51 -0500

    Anexo integrantes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ function App() {
  return(
     <Router>
       <div className="container">
-        <div class="card">
-          <div class="card-header">
+        <div className="card">
+          <div className="card-header">
             <b>Sistema de Ventas</b>
           </div>
-          <div class="card-body">
+          <div className="card-body">
             <Route component={Login}></Route>
+          </div>
+          <div className="card-footer">
+            <small>Keydi Mercado<br/>
+            Andrea Zorro<br/>
+            Carolina Mantilla</small>
           </div>
         </div>
       </div>
